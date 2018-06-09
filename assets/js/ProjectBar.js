021cf574bc0d547ce452042ff41a684a303a7b5b
AcelisWeaven <jeremy.graziani@gmail.com>
2018-06-09 22:53:19 +0200

    Auto updating project-bar

--- assets/js/ProjectBar.js
+++ assets/js/ProjectBar.js
@@ -1,4 +1,5 @@
 const Utilities = require("./Utilities");
+const QRCode = require("qrcodejs2");
 
 module.exports = class Board {
     constructor(parent) {
@@ -6,6 +7,8 @@ module.exports = class Board {
         this.projects = null;
         this.selector = document.getElementById("project");
         this.projectBar = document.querySelector(".project-bar");
+        this.qrcode = null;
+        this.qrcodeElem = document.getElementById("qrcode");
         this.projectNameElem = document.querySelector(".project-name");
         this.projectAddressElem = document.querySelector(".project-address");
         this.pixelMessageElem = document.querySelector(".pixel-message");
@@ -57,12 +60,43 @@ module.exports = class Board {
             return;
         }
 
+        const pixelID = this.getPixelID();
         const selectedProject = this.projects.filter(elem => {
             return elem.address === this.selector.value;
         })[0];
         this.projectNameElem.innerText = selectedProject.project;
         this.projectAddressElem.innerText = selectedProject.address;
+        this.pixelMessageElem.innerText = pixelID;
         this.projectBar.classList.remove("hidden");
+        this.makeQrCode(JSON.stringify({
+            address: selectedProject.address,
+            message: `pixiota ${pixelID}`,
+        }));
     }
 
+    makeQrCode(text) {
+        if (!this.qrcode) {
+            this.qrcode = new QRCode(this.qrcodeElem, {
+                text,
+                width: 256,
+                height: 256,
+                colorDark : "#2f3a43",
+            });
+        } else {
+            this.qrcode.makeCode(text);
+        }
+    }
+
+    /**
+     * PixelID is made this following way, where parts are separated with a dot:
+     * First part (0-f) is hex representation of the color
+     * Second part represents the position on the board (0-9a-z)
+     */
+    getPixelID() {
+        const pixelNumber = ((pt) => {
+            return pt.x + pt.y * this.parent.board.size
+        })(this.parent.previewPixelLockBoardPos);
+        const color = parseInt(this.parent.board.currentColor.getAttribute("data-color-index"));
+        return `${color.toString(16)}.${pixelNumber.toString(36)}`;
+    }
 };
